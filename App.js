71a747b578582ebf987d8986e075acf56e83bd26
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-27 23:16:35 -0600

    react navigation with redux is working oh both ios and android! :D

--- App.js
+++ App.js
@@ -1,53 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import {
-  AppRegistry,
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+
 import AppReducer from './src/reducers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
 
+class ReduxExampleApp extends React.Component {
+  store = createStore(AppReducer);
 
-
-export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, scream loudly into a pillow
-        </Text>
-      </View>
+      <Provider store={this.store}>
+        <AppWithNavigationState />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+AppRegistry.registerComponent('ReduxExample', () => ReduxExampleApp);
+
+export default ReduxExampleApp;
